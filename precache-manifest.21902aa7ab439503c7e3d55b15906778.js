4eac14a522d2ccc514f289a296740bd319e03d08
llkaplan <leylakaplan@leylas-mbp.lan>
2020-02-20 14:14:31 -0800

    Updates

---
+++ precache-manifest.21902aa7ab439503c7e3d55b15906778.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3810c90ffb6fa4c50b138c5ad84e8836",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c116fb4f6b0cf946e669",
+    "url": "/portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c1a2264a07d37452282e",
+    "url": "/portfolio/static/css/main.7253b48c.chunk.css"
+  },
+  {
+    "revision": "c116fb4f6b0cf946e669",
+    "url": "/portfolio/static/js/2.60065d79.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.60065d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a2264a07d37452282e",
+    "url": "/portfolio/static/js/main.97cd54b2.chunk.js"
+  },
+  {
+    "revision": "457d02271373da8dec5e",
+    "url": "/portfolio/static/js/runtime-main.fba2446e.js"
+  },
+  {
+    "revision": "22ec538a1e7c6588e567e2bfacaa5583",
+    "url": "/portfolio/static/media/Github.22ec538a.png"
+  },
+  {
+    "revision": "9332e4e7bccf3da6e82d291aa9d8573a",
+    "url": "/portfolio/static/media/bookworm.9332e4e7.png"
+  },
+  {
+    "revision": "dd63ee47c5079fd7838ca1c528c914cf",
+    "url": "/portfolio/static/media/codingquiz.dd63ee47.png"
+  },
+  {
+    "revision": "5ed9c868b7b3b9e6d01045a143fbf7dc",
+    "url": "/portfolio/static/media/employeeapp.5ed9c868.png"
+  },
+  {
+    "revision": "bbf55b31376305aff38a57d79d5c61e3",
+    "url": "/portfolio/static/media/employeemanagement.bbf55b31.png"
+  },
+  {
+    "revision": "4dcd6aa48529b067ae95756768fefd0f",
+    "url": "/portfolio/static/media/me.4dcd6aa4.jpeg"
+  },
+  {
+    "revision": "5a4d9a3d0b7643f6e4b8e76ca9285159",
+    "url": "/portfolio/static/media/passwordgenerator.5a4d9a3d.png"
+  },
+  {
+    "revision": "70d87492340ba9a8528371cc78f305dc",
+    "url": "/portfolio/static/media/project2.70d87492.png"
+  }
+]);
